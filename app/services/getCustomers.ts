cd954ebaa60b83ce258e73f5911811e07d6e30fc
Ingrid <ingridstanic@gmail.com>
2026-08-13 14:54:06 +0200

    implement functions to fetch data from API

---
+++ app/services/getCustomers.ts
@@ -0,0 +1,20 @@
+import { Customer } from "../models/Customer";
+
+export const getCustomers = async () => {
+  const apiUrl =
+    "https://hotelapi-efatf0cfevcgb5gd.swedencentral-01.azurewebsites.net/customers";
+
+  try {
+    const response = await fetch(apiUrl);
+
+    if (!response.ok) {
+      console.error("Could not get data.");
+    }
+
+    const data: Customer[] = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Could not fetch data. ", error);
+  }
+};
