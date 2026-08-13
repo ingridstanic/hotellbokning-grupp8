99f947f16e13b735edcc32e5cfff6b421ee4e570
Erika Hörling <erikahorling@mbpsomtorerika2.home>
2026-08-13 13:06:28 +0200

    Link gästlista

--- src/sections/BookingSection.tsx
+++ src/sections/BookingSection.tsx
@@ -1,16 +1,9 @@
-import Link from "next/link";
-
 export default function BookingSection() {
   return (
     <section className="min-h-screen bg-[#FFF9F3]">
       <h2 className="font-display text-center text-5xl text-black">
         Boka
       </h2>
-      <div className="mt-8 flex justify-center">
-        <Link href="/guests/${guestId}" className="rounded bg-[#74645B] px-4 py-2 text-black hover:bg-[#5C4D44]">
-          Registrera ny gäst
-        </Link>
-      </div>
     </section>
   );
 }
--- src/sections/GuestSection.tsx
+++ src/sections/GuestSection.tsx
@@ -1,29 +1,56 @@
+ import Link from "next/link";
+ 
  export default function GuestSection() {
   return (
-    <main className="min-h-screen bg-[#FFF9F3]">
+    <main className="min-h-screen border-50 border-[#74645B] bg-[#FFF9F3]">
       <h1 className="font-display text-center text-5xl py-30 text-black">
         Gästlista
       </h1>
 
     <div className="mx-auto h-px w-[80%] bg-black/30" />
 
-   
-    <p className="text-left text-black py-8 pl-40">
-        Namn: 
-    </p>
+ <div className="flex items-center justify-between px-40 py-8">
+  <p className="text-left text-black">
+    Namn:
+  </p>
+
+  <Link
+    href="/guests/${guestId}"
+    className="flex h-10 w-10 items-center justify-center rounded-full bg-[#85756B] text-black hover:bg-[#5C4D44]"
+  >
+    ⋮
+  </Link>
+</div>
 
      <div className="mx-auto h-px w-[80%] bg-black/30" />
 
-    <p className="text-left text-black py-8 pl-40">
-        Namn:
-        </p>
+    <div className="flex items-center justify-between px-40 py-8">
+  <p className="text-left text-black">
+    Namn:
+  </p>
+
+  <Link
+    href="/guests/${guestId}"
+    className="flex h-10 w-10 items-center justify-center rounded-full bg-[#85756B] text-black hover:bg-[#5C4D44]"
+  >
+    ⋮
+  </Link>
+</div>
 
      <div className="mx-auto h-px w-[80%] bg-black/30" />
 
-    <p className="text-left text-black py-8 pl-40">
-        Namn:
-    </p>
+    <div className="flex items-center justify-between px-40 py-8">
+  <p className="text-left text-black">
+    Namn:
+  </p>
 
+  <Link
+    href="/guests/${guestId}"
+    className="flex h-10 w-10 items-center justify-center rounded-full bg-[#85756B] text-black hover:bg-[#5C4D44]"
+  >
+    ⋮
+  </Link>
+</div>
     </main>
 
   );
