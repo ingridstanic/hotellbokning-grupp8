9b67c71a7097bb83a268ee9e24b24e40b5cce5b1
Erika Hörling <erikahorling@mbpsomtorerika2.home>
2026-08-14 09:46:06 +0200

    cleaned page

--- src/app/guests/[guestId]/page.tsx
+++ src/app/guests/[guestId]/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import GuestProfile from "@/src/sections/GuestProfileSection";
 import Navbar from "@/src/components/Navbar/Navbar";
 import Footer from "@/src/components/Footer/Footer";
 
@@ -10,83 +10,11 @@ export default async function GuestProfilePage({
   const { guestId } = await params;
 
   return (
-
     <main className="min-h-screen bg-[#FFF9F3]">
-        <Navbar />
-      <h1 className="font-display text-center text-5xl pt-50 text-black">
-        Gästprofil
-      </h1>
-
-      <p className="text-center text-black">
-        Gäst: {guestId}
-      </p>
-<div className="absolute left-20 top-20">
-      <Image
-      src="/images/hotell.png"
-      alt="Hotell"
-      width={300}
-      height={300}
-    />
-</div>
-     <p className="text-right text-2xl pr-40 py-10 text-black">
-        Namn: {guestId}
-    </p>
-
-    <div className="mx-auto h-px w-[80%] bg-black/30" />
-
-    <div className="flex justify-end pr-40 pt-10">
-      <button className="rounded-md border border-black px-7 py-3 text-black">
-        Spara
-      </button>
-    </div>
-
-     <p className="text-left text-black py-5 pl-40">
-        Förnamn: 
-    </p>
-
-    <p className="text-left text-black py-5 pl-40">
-        Efternamn:
-    </p>
-
-    <p className="text-left text-black py-5 pl-40">
-        Email:
-    </p>
-
-<p className="text-left text-black text-2xl py-2 pt-10 pl-40">
-        Bokningar
-    </p>
-
-    <div className="mx-auto h-px w-[80%] bg-black/30" />
-
-    <div className="mx-auto flex w-[80%] items-center justify-between pt-10 py-20">
-  <div>
-    <p>Check in:</p>
-    <p>2026-08-12</p>
-  </div>
-
-  <div>
-    <p>Check out:</p>
-    <p>2026-08-14</p>
-  </div>
-
-  <div>
-    <p>Antal gäster:</p>
-    <p>2</p>
-  </div>
-
-  <button className="rounded-md border border-black px-5 py-2">
-    Ändra
-  </button>
-
-  <button className="rounded-md border border-black px-5 py-2">
-    Avboka
-  </button>
-
-  <button className="rounded-md border border-black px-5 py-2">
-    Check In
-  </button>
-</div>
-<Footer />
+      <Navbar />
+      <GuestProfile guestId={guestId} />
+      <Footer />
     </main>
   );
 }
+  
---
+++ src/sections/GuestProfileSection.tsx
@@ -0,0 +1,85 @@
+import Image from "next/image";
+
+type GuestProfileSectionProps = {
+    guestId: string;
+}
+
+export default function GuestProfile({ guestId }: GuestProfileSectionProps) {
+    return (
+        <main className="min-h-screen bg-[#FFF9F3]">
+            <h1 className="font-display text-center text-5xl pt-50 text-black">
+                Gästprofil
+            </h1>
+
+            <p className="text-center text-black">
+                Gäst: {guestId}
+            </p>
+            <div className="absolute left-20 top-20">
+                <Image
+                    src="/images/hotell.png"
+                    alt="Hotell"
+                    width={300}
+                    height={300}
+                />
+            </div>
+            <p className="text-right text-2xl pr-40 py-10 text-black">
+                Namn: {guestId}
+            </p>
+
+            <div className="mx-auto h-px w-[80%] bg-black/30" />
+
+            <div className="flex justify-end pr-40 pt-10">
+                <button className="rounded-md border border-black px-7 py-3 text-black">
+                    Spara
+                </button>
+            </div>
+
+            <p className="text-left text-black py-5 pl-40">
+                Förnamn:
+            </p>
+
+            <p className="text-left text-black py-5 pl-40">
+                Efternamn:
+            </p>
+
+            <p className="text-left text-black py-5 pl-40">
+                Email:
+            </p>
+
+            <p className="text-left text-black text-2xl py-2 pt-10 pl-40">
+                Bokningar
+            </p>
+
+            <div className="mx-auto h-px w-[80%] bg-black/30" />
+
+            <div className="mx-auto flex w-[80%] items-center justify-between pt-10 py-20">
+                <div>
+                    <p>Check in:</p>
+                    <p>2026-08-12</p>
+                </div>
+
+                <div>
+                    <p>Check out:</p>
+                    <p>2026-08-14</p>
+                </div>
+
+                <div>
+                    <p>Antal gäster:</p>
+                    <p>2</p>
+                </div>
+
+                <button className="rounded-md border border-black px-5 py-2">
+                    Ändra
+                </button>
+
+                <button className="rounded-md border border-black px-5 py-2">
+                    Avboka
+                </button>
+
+                <button className="rounded-md border border-black px-5 py-2">
+                    Check In
+                </button>
+            </div>
+        </main>
+    );
+}
