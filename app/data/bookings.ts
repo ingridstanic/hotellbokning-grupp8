22fa838c3257efc2311cf2f12fd0c90bebef209a
Ingrid <ingridstanic@gmail.com>
2026-08-14 11:48:59 +0200

    implement data for bookings

--- app/data/bookings.ts
+++ app/data/bookings.ts
@@ -1,1 +1,39 @@
-export const seedBookings = [];
+import { NewBooking } from "../models/NewBooking";
+
+export const seedBookings: NewBooking[] = [
+  {
+    hotelId: 23,
+    customerId: 249,
+    checkInDate: "2027-08-14",
+    checkOutDate: "2027-08-16",
+    guests: 2,
+  },
+  {
+    hotelId: 23,
+    customerId: 247,
+    checkInDate: "2026-10-14",
+    checkOutDate: "2026-20-20",
+    guests: 1,
+  },
+  {
+    hotelId: 23,
+    customerId: 252,
+    checkInDate: "2026-10-18",
+    checkOutDate: "2026-10-20",
+    guests: 3,
+  },
+  {
+    hotelId: 23,
+    customerId: 251,
+    checkInDate: "2026-10-18",
+    checkOutDate: "2026-11-02",
+    guests: 2,
+  },
+  {
+    hotelId: 23,
+    customerId: 250,
+    checkInDate: "2026-12-20",
+    checkOutDate: "2026-12-26",
+    guests: 1,
+  },
+];
