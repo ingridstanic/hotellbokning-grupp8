23714c2ca8e6e5a3f094fb20b9b9a055e84ef438
Erika Hörling <erikahorling@mbpsomtorerika2.home>
2026-08-17 11:35:24 +0200

    logga

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,6 +1,16 @@
+import Image from "next/image";
+
 export default function Navbar() {
   return (
     <nav className="absolute top-0 left-0 z-20 w-full bg-[#74645B]">
+      <div className="flex h-15 items-center justify-start px-8 pt-10">
+        <Image
+          src="/images/logga.png"
+          alt="Logo"
+          width={100}
+          height={40}
+        />
+      </div>
       <div className="flex h-15 items-center justify-end px-8">
         <div className="flex h-full items-center">
           <a
