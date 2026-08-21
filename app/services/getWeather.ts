1149e9ba363f25965bfc51059b9e045f7f4a18b5
Ingrid <ingridstanic@gmail.com>
2026-08-21 10:00:14 +0200

    implement try catch

--- app/services/getWeather.ts
+++ app/services/getWeather.ts
@@ -1,12 +1,19 @@
+import { WeatherResponse } from "../models/WeatherResponse";
+
 export async function getWeather() {
+  try {
     const response = await fetch(
-    "https://api.open-meteo.com/v1/forecast?latitude=59.3293&longitude=18.0686&daily=temperature_2m_max,temperature_2m_min,weather_code&timezone=Europe%2FStockholm&past_days=1"
-);
+      "https://api.open-meteo.com/v1/forecast?latitude=59.3293&longitude=18.0686&daily=temperature_2m_max,temperature_2m_min,weather_code&timezone=Europe%2FStockholm&past_days=1",
+    );
 
     if (!response.ok) {
-        throw new Error("Failed to fetch weather data");
+      throw new Error(`Could not load weather. ${response.status}`);
     }
 
-    const data = await response.json();
+    const data: WeatherResponse = await response.json();
     return data;
+  } catch (error) {
+    console.error("Could not fetch weather data, ", error);
+    return null;
+  }
 }
